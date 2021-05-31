872335dbf5d839b817ec1b9773f5cd6ac2ac2d4f
AirCaptainx99 <80711605+AirCaptainx99@users.noreply.github.com>
2021-05-31 15:00:35 +0700

    Delete isLunarYear.js

--- files/functions/isLunarYear.js
+++
@@ -1,8 +0,0 @@
-String.prototype.isLunarYear = function() {
-    if ((this % 4 == 0 && this % 100 != 0) || this % 400 == 0) return true;
-    else return false;
-}
-
-module.exports = {
-    isLunarYear,
-}
