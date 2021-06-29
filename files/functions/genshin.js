33c2049f268b7f78f9f236dff69e84ae4a54f46a
AirCaptainx99 <jeffryco_ardiya@yahoo.com>
2021-06-29 20:24:03 +0700

    ngerapiin function genshin bitch!

--- files/functions/genshin.js
+++ files/functions/genshin.js
@@ -1,44 +1,106 @@
 const { MessageEmbed } = require('discord.js');
 const { Discord } = require('../core_module.js');
+
+const freedom = ["Amber", "Barbara", "Klee", "Sucrose", "Childe", "Diona"];
+const prosperity = ["Keqing", "Ningguang", "Qiqi", "Xiao"];
+const resistance = ["Bennet", "Diluc", "Jean", "Mona", "Noelle", "Razor", "Eula"];
+const diligence = ["Chongyun", "Xiangling", "Ganyu", "Hu Tao", "Kazuha"];
+const ballad = ["Fischl", "Kaeya", "Lisa", "Venti", "Albedo", "Rosaria"];
+const gold = ["Beidou", "Xingqiu", "Zhongli", "Xinyan", "Yanfei"];
+
 const genshin = async function (msg, day){
+    let books = ['', ''];
+    let mondstadt = '';
+    let liyue = '';
+
     let message = new Discord.MessageEmbed()
     .setColor('#FFFFFF')
     .setTitle('Genshin Farmables');
     switch (day){
         case 1 :
             message.setDescription('**Monday**');
-            message.addField('Mondstadt Talent: Freedom', 'Amber\nBarbara\nKlee\nSucrose\nChilde\nDiona');
-            message.addField('Liyue Talent: Prosperity', 'Keqing\nNingguang\nQiqi\nXiao');
+            books[0] = 'Freedom';
+            books[1] = 'Prosperity';
+            for (let i = 0; i < freedom.length; i++){ // mondstadt
+                mondstadt += freedom[i];
+                if (i != freedom.length - 1) mondstadt += '\n';
+            }
+            for (let i = 0; i < prosperity.length; i++){ // liyue
+                liyue = prosperity[i];
+                if (i != prosperity.length - 1) liyue += '\n';
+            }
             break;
         case 2 :
             message.setDescription('**Tuesday**');
-            message.addField('Mondstadt Talent: Resistance', 'Bennet\nDiluc\nJean\nMona\nNoelle\nRazor\nEula');
-            message.addField('Liyue Talent: Diligence', 'Chongyun\nXiangling\nGanyu\nHu Tao');
+            books[0] = 'Resistance';
+            books[1] = 'Diligence';
+            for (let i = 0; i < resistance.length; i++){ // mondstadt
+                mondstadt += resistance[i];
+                if (i != resistance.length - 1) mondstadt += '\n';
+            }
+            for (let i = 0; i < diligence.length; i++){ // liyue
+                liyue += diligence[i];
+                if (i != diligence.length - 1) liyue += '\n';
+            }
             break;
         case 3 :
             message.setDescription('**Wednesday**');
-            message.addField('Mondstadt Talent: Ballad', 'Fischl\nKaeya\nLisa\nVenti\nAlbedo\nRosaria');
-            message.addField('Liyue Talent: Gold', 'Beidou\nXingqiu\nZhongli\nXinyan\nYanfei');
+            books[0] = 'Ballad';
+            books[1] = 'Gold';
+            for (let i = 0; i < ballad.length; i++){ // mondstadt
+                mondstadt += ballad[i];
+                if (i != ballad.length - 1) mondstadt += '\n';
+            }
+            for (let i = 0; i < gold.length; i++){ // liyue
+                liyue += gold[i];
+                if (i != gold.length - 1) liyue += '\n';
+            }
             break;
         case 4 :
             message.setDescription('**Thursday**');
-            message.addField('Mondstadt Talent: Freedom', 'Amber\nBarbara\nKlee\nSucrose\nChilde\nDiona');
-            message.addField('Liyue Talent: Prosperity', 'Keqing\nNingguang\nQiqi\nXiao');
+            books[0] = 'Freedom';
+            books[1] = 'Prosperity';
+            for (let i = 0; i < freedom.length; i++){ // mondstadt
+                mondstadt += freedom[i];
+                if (i != freedom.length - 1) mondstadt += '\n';
+            }
+            for (let i = 0; i < prosperity.length; i++){ // liyue
+                liyue = prosperity[i];
+                if (i != prosperity.length - 1) liyue += '\n';
+            }
             break;
         case 5 :
             message.setDescription('**Friday**');
-            message.addField('Mondstadt Talent: Resistance', 'Bennet\nDiluc\nJean\nMona\nNoelle\nRazor\nEula');
-            message.addField('Liyue Talent: Diligence', 'Chongyun\nXiangling\nGanyu\nHu Tao');
+            books[0] = 'Resistance';
+            books[1] = 'Diligence';
+            for (let i = 0; i < resistance.length; i++){ // mondstadt
+                mondstadt += resistance[i];
+                if (i != resistance.length - 1) mondstadt += '\n';
+            }
+            for (let i = 0; i < diligence.length; i++){ // liyue
+                liyue += diligence[i];
+                if (i != diligence.length - 1) liyue += '\n';
+            }
             break;
         case 6 :
             message.setDescription('**Saturday**');
-            message.addField('Mondstadt Talent: Ballad', 'Fischl\nKaeya\nLisa\nVenti\nAlbedo\nRosaria');
-            message.addField('Liyue Talent: Gold', 'Beidou\nXingqiu\nZhongli\nXinyan\nYanfei');
+            books[0] = 'Ballad';
+            books[1] = 'Gold';
+            for (let i = 0; i < ballad.length; i++){ // mondstadt
+                mondstadt += ballad[i];
+                if (i != ballad.length - 1) mondstadt += '\n';
+            }
+            for (let i = 0; i < gold.length; i++){ // liyue
+                liyue += gold[i];
+                if (i != gold.length - 1) liyue += '\n';
+            }
             break;
         case 0 :
             message.setDescription('**Kebuka Semua Goblock**');
             break;
     }
+    message.addField('Mondstadt Talent: ' + books[0], mondstadt);
+    message.addField('Liyue Talent: ' + books[1], liyue);
     msg.channel.send(message);
 }
 
